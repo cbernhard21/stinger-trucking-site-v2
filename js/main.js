ba4274bcc465cb5c0bbd5c7e5b9837c6fcdb9736
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-08-16 20:24:53 +0000

    changes to wording from Kim

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ import { createFooter } from '../components/footer.js';
 
 
 function insertPhoneNumber() {
-  const phoneNumber = '123-555-5555';
-
+  const phoneNumber = '989-614-2783';
   const phoneNumberContainer = document.querySelectorAll('.phoneNumberContainer');
-
   phoneNumberContainer.forEach((container) => {
     container.textContent = phoneNumber;
   });
-
   return phoneNumber;
 }
 

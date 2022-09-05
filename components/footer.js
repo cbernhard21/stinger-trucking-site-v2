b8303805f4553a8e8664d4d1aa974d7b8d4fcd92
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-09-05 01:19:32 +0000

    style contact page

--- components/footer.js
+++ components/footer.js
@@ -5,35 +5,119 @@ export function createFooter(phoneNumber) {
 
         <style>
           footer {
+            width: 95%;
+            margin: 0 auto;
+            padding: 4rem 0;
+            font-size: 1.4rem;
             background-color: var(--black);
-            color: var(--white);
-            padding: 2rem 0;
-            font-size: 1.6rem;
-            width: 100%;
+            color: var(--faded-white);
             display: flex;
             justify-content: space-between;
+          }
+
+          @media screen and (max-width: 550px) {
+            footer {
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+          }
+
+          a,
+          a:visited,
+          a:link {
+            color: var(--faded-white);
+            text-decoration: none;
           }
 
           .footer-copyright {
             text-align: center;
           }
 
+          h2 {
+            color: var(--white);
+            font-family: var(--ff-inter);
+            font-weight: var(--fw-300);
+            font-size: 2rem;
+            margin-bottom: 1rem;
+          }
+
+          @media screen and (max-width: 550px) {
+            h2 {
+              text-align: center;
+              margin-bottom: 0;
+            }
+          }
+
+          ul {
+            list-style-type: none;
+          }
+
+          @media screen and (max-width: 550px) {
+            ul {
+              display: flex;
+              gap: 1.5rem;
+            }
+          }
+
+          li {
+            margin-bottom: 1rem;
+          }
+
+          .footer-copyright {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+          }
+
           .phoneNumberContainer {
             text-decoration: none;
             display: block;
-            color: var(--white);
           } 
+
+          @media screen and (max-width: 550px) {
+            .hidden {
+              display: none;
+            }
+            .footer-contact {
+              text-align: center;
+              order: 1;
+            }
+
+            .footer-nav {
+              order: 2;
+            }
+
+            .footer-copyright {
+              order: 3;
+            }
+
+
+            .footer-contact,
+            .footer-nav {
+              margin-bottom: 2rem;
+            }
+          }
+
         </style>
 
         <link rel="stylesheet" href="../styles/component-reset.css" />
         <footer>
         
-          <div class="footer-contact"></div>
-          <div class="footer-copyright">          
-            <p>Stinger Trucking Company</p>
+          <div class="footer-contact">
+            <h2>Contact Us</h2>
             <a class="phoneNumberContainer" href="tel:${phoneNumber}">${phoneNumber}</a>
-            <p>Copyright 2022</p>
+            <p>Address</p>
+            <p>Email</p>
           </div>
+
+          <div class="footer-copyright">   
+            <img src="../images/logo-company-name-bee.png" alt="Logo" class="hidden" />
+            <p>&copy <span class="year">2022</span> Stinger Trucking</p>
+            <p>Design and Maintained By Left Hand Productions</p>
+          </div>
+
           <div class="footer-nav">
             <h2>Site Map</h2>
             <ul>
@@ -43,6 +127,7 @@ export function createFooter(phoneNumber) {
               <li><a href="contact.html">Contact</a></li>
             </ul>
           </div>
+
         </footer>
     `;
 
@@ -54,8 +139,11 @@ export function createFooter(phoneNumber) {
       const shadowRoot = this.attachShadow({ mode: 'open' });
       shadowRoot.appendChild(footerTemplate.content.cloneNode(true));
       const homeLink = this.shadowRoot.querySelector('.home');
+      const yearContainer = this.shadowRoot.querySelector('.year');
+      const date = new Date();
+      let year = date.getFullYear();
 
-
+      yearContainer.innerText = year;
 
       let activePage = window.location.pathname;
       if(activePage === '/' || activePage === '/index.html') {
